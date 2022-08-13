64d1e589391b77363866f53573a1ccd41aef238e
taha <taha@gmail.com>
2022-08-13 13:21:14 +0500

    full & final

---
+++ resources/js/Pages/Chat/container.vue
@@ -0,0 +1,101 @@
+<template>
+   <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <chat-room-selection 
+                v-if="currentRoom.id"
+                :rooms="chatRooms"
+                :currentRoom="currentRoom"
+                v-on:roomchanged="setRoom($event)"
+                 />
+            </h2>
+        </template>
+
+        <div class="py-12 ">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ">
+                    <message-container :messages="messages"/>
+                    <input-message :room="currentRoom" v-on:messagesent="getMessages()"/>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+    </template>
+
+<script >
+    import AppLayout from '@/Layouts/AppLayout.vue';
+    import messageContainer from './messageContainer.vue';
+    import inputMessage from './inputMessage.vue';
+import axios from 'axios';
+import ChatRoomSelection from './chatRoomSelection.vue';
+export default {
+components:{
+    AppLayout,
+    messageContainer,
+    inputMessage,
+    ChatRoomSelection,
+}, 
+  data: function(){
+      return {
+          chatRooms:[],
+          currentRoom:[],
+          messages:[]
+      }
+  },
+  watch:{
+currentRoom(val, oldVal){
+    if(oldVal.id){
+        this.disconnect(oldVal)
+    }
+    this.connect(); 
+}
+  },
+  methods:{
+      connect(){
+        if(this.currentRoom.id){
+            let vm = this;
+            this.getMessages();
+            window.Echo.private("chat."+ this.currentRoom.id)
+            .listen('NewChatMessage', e => {
+                vm.getMessages();
+            })
+        }
+      },
+        disconnect(room){
+   window.Echo.leave("chat."+room.id);
+    },
+    
+    setRoom(room){
+        this.currentRoom = room ;
+    },
+    getRoom(){
+        axios.get('/chat/rooms')
+        .then(response => {
+            this.chatRooms=  response.data;
+            this.setRoom(response.data[0]);
+        })
+
+        .catch(error => {
+            console.log(error);
+        })
+    },
+  
+    getMessages(){
+   axios.get('/chat/room/'+ this.currentRoom.id + '/messages')
+   .then(response =>{
+       this.messages = response.data;
+   } )
+    .catch(error => {
+            console.log(error);
+        })
+    }
+  },
+  created(){
+      this.getRoom();
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Chat/messageItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div> 
+  {{message.user.name}}<b>:</b>   {{ message.message}}
+
+  </div>
+</template>
+
+<script>
+export default {
+props:['message'],
+}
+</script>
+
+<style>
+
+</style>
